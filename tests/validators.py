33d25ad7da5d5abdee1f03129f2cec088d5270f4
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2022-08-24 17:37:18 +0300

    fix metallb tests

--- tests/validators.py
+++ tests/validators.py
@@ -342,7 +342,9 @@ def validate_metallb_config(ip_ranges="192.168.0.105"):
     if platform.machine() != "x86_64":
         print("Metallb tests are only relevant in x86 architectures")
         return
-    out = kubectl("get configmap config -n metallb-system -o jsonpath='{.data.config}'")
+    out = kubectl(
+        "get ipaddresspool -n metallb-system default-addresspool -o jsonpath='{.spec.addresses}"
+    )
     for ip_range in ip_ranges.split(","):
         assert ip_range in out
 

be866215ea5407c19da88802d8bb6299484785f4
Stéphane Talbot <Stephane.Talbot@univ-savoie.fr>
2024-07-24 09:25:43 +0200

    Remove old enable syntax in metallb test

--- tests/test-addons.py
+++ tests/test-addons.py
@@ -276,7 +276,7 @@ class TestAddons(object):
             "192.168.0.105-192.168.0.105,192.168.0.110-192.168.0.111,192.168.1.240/28,fd00:db8:1::/64,fd01:db9::-fd01:db9::1:FFFF"
         )
         print("Enabling metallb")
-        microk8s_enable("{}:{}".format(addon, ip_ranges), timeout_insec=500)
+        microk8s_enable("{} {}".format(addon, ip_ranges), timeout_insec=500)
         validate_metallb_config(ip_ranges)
         print("Disabling metallb")
         microk8s_disable("metallb")
